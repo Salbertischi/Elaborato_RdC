e72704a54b3508347d0d8dbb45177a766904ea41
Salbertischi <152185117+Salbertischi@users.noreply.github.com>
2023-12-04 11:49:24 +0100

    Update Elab.py

--- Elab.py
+++ Elab.py
@@ -1,11 +1,28 @@
 from func import *
+import os
 
 
 def main():
-    analyze_traffic('traffic.pcap')
+    pcaps = []
+    outfiles = []
+    directories = os.listdir('../02_15_b2_00_00_00')
+    for dir in directories:
+        path = f'../02_15_b2_00_00_00/{dir}/traffic.pcap'
+        pcaps.append(path)
+        out_file = f'./logs/{dir}.log'
+        outfiles.append(out_file)
+    #print(pcaps)
+    #print(len(pcaps))
+    #print(outfiles)
+    #print(len(outfiles))
+
+
+    for pcap, out in zip(pcaps, outfiles):  
+        analyze_traffic(pcap, out)
 
 
 if __name__ == '__main__':
+    pcacp_speciale = '../02_15_b2_00_00_00/20231109215429/traffic.pcap'
     main()
 
 
