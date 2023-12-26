17299e63e6334e5e9d9b9471fd79b3d7f1a8f793
Salbertischi <152185117+Salbertischi@users.noreply.github.com>
2023-12-26 10:05:50 +0100

    Update Elaborato.py

--- Elaborato.py
+++ Elaborato.py
@@ -1,16 +1,28 @@
 from functions import *
 import os
+import subprocess
+
+
+def addCapsInfos(pcap, out):
+    info = subprocess.run(['capinfos', pcap], capture_output=True, text=True, check=True)
+
+    with open(out, 'w') as file:
+        file.write(info.stdout)
+
 
 def main():
     # Trova le directories di tutti i file pcap e crea un file log associato
     pcaps = []
     outfiles = []
+    infoFiles = []
     directories = os.listdir('../02_15_b2_00_00_00')
     for dir in directories:
         path = f'../02_15_b2_00_00_00/{dir}/traffic.pcap'
         pcaps.append(path)
         out_file = f'./logs/{dir}.log'
         outfiles.append(out_file)
+        info = f'./capsInfos/{dir}.infos'
+        infoFiles.append(info)
 
     # Analizza le tracce pcap e metti il risultato nei file log associati
     i = 1
@@ -18,6 +30,13 @@ def main():
         print(f"Traccia {i}")
         analizzaPCAP(pcap, out)
         i += 1
+    # Aggiungi i risultati di capinfos
+    i = 1
+    for pcap, out in zip(pcaps, infoFiles):
+        print(f"CapInfos {i}")
+        addCapsInfos(pcap, out)
+        i += 1
+
 
 
 if __name__ == '__main__':
